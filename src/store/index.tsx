fdfc8a8bb9d1101b10b89a91099492abf3dd1326
gis4tech-anda-ugr <121191313+gis4tech-anda-ugr@users.noreply.github.com>
2023-11-07 12:51:57 +0100

    Update index.tsx
    Modificación de:
    C:\Users\Usuario\Documents\GitHub\visor-dugra\src\store\index.tsx

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -5,7 +5,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import layer from "./slices/layer/index";
 import data from "./slices/data/index";
 import basemap from "./slices/basemap";
-import baselayer from "./slices/baselayer";
 import mvt from "./slices/mvt";
 import type from "./slices/type";
 import style from "./slices/style";
@@ -17,7 +16,6 @@ export default configureStore({
     layer,
     data,
     basemap,
-    baselayer,
     mvt,
     type,
     style,
