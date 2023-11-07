c1fd331c4880d69e0b031a8b9d640fd40db80446
gis4tech-visor-dugra <147627859+gis4tech-visor-dugra@users.noreply.github.com>
2023-11-07 11:38:58 +0100

    Add files via upload

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -5,7 +5,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import layer from "./slices/layer/index";
 import data from "./slices/data/index";
 import basemap from "./slices/basemap";
-import baselayer from "./slices/baselayer";
 import mvt from "./slices/mvt";
 import type from "./slices/type";
 import style from "./slices/style";
