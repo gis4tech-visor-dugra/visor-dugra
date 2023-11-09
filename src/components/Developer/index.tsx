b6c6ad3a774a2b818dc7143970bf46b377b848d1
gis4tech-anda-ugr <121191313+gis4tech-anda-ugr@users.noreply.github.com>
2023-11-09 13:10:38 +0100

    Update index.tsx
    Developer

--- src/components/Developer/index.tsx
+++ src/components/Developer/index.tsx
@@ -12,9 +12,6 @@ export default function ImageAvatars() {
       <Link href="https://www.gis4tech.com/" target="_blank">
         <img src="/images/gis4tech.jpg" alt="gis4tech" width="auto" height="40px" />
       </Link>
-      <Link href="https://www.geopois.com/" target="_blank">
-        <img src="/images/geopois.png" alt="geopois" width="auto" height="40px" />
-      </Link>
     </Stack>
   );
 }
