32d4fd0f793022812aa4e6a93380d3437d4ff3f1
gis4tech-anda-ugr <121191313+gis4tech-anda-ugr@users.noreply.github.com>
2023-11-09 15:30:01 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,9 +20,9 @@ const Map = dynamic(() => import('../components/Map/Map'), {
   ssr: false
 });
 
-const MapMalaga = dynamic(() => import('../components/MapMalaga/Map'), {
-  ssr: false
-});
+// const MapMalaga = dynamic(() => import('../components/MapMalaga/Map'), {
+//   ssr: false
+// });
 // Components
 import Select from "../components/Select/Indicadores";
 import SelectBasemap from "../components/Select/BaseMaps";
@@ -45,40 +45,40 @@ export default function App() {
     }
   };
 
-  return (
-    <Box sx={{ width: '100%', typography: 'body1' }}>
-      <TabContext value={value}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider', display: 'flex', justifyContent: 'center' }}>
-          <TabList onChange={handleChange} aria-label="lab API tabs example">
-            <Tab label="Granada" value="Granada" />
-            <Tab label="Málaga" value="Malaga" />
-          </TabList>
-        </Box>
-        <TabPanel value="Granada">
-        </TabPanel>
-        <TabPanel value="Malaga">
-          < MapMalaga />
-        </TabPanel>
-      </TabContext>
-      <div style={{ position:'absolute', top:'10px', right:'300px' }} >
-        <div style={{ display:'flex', justifyContent:'center', alignItems:'center' }}>
-          <Typography><b>{title} ({units})</b></Typography>
-        </div>
-      </div>
-      <div style={{ position:'absolute', top:'0px', right:'20px' }} >
-        <div style={{ display:'flex', justifyContent:'center', alignItems:'center' }}>
-          <Typography variant="caption" color="text.secondary" align="center">Financiado por </Typography>
-          <Sponsor />
-        </div>
-      </div>
-      <div style={{ position:'absolute', top:'0px', left:'20px',margin:'2px' }} >
-          <Developer />
-      </div>
-      <Box sx={{ position:'absolute', bottom:'60px', left:'20px'}}>
-        <CardInfo />
-        <Select />
-        <SelectBasemap />
-      </Box>
-    </Box>
-  );
-}
+//   return (
+//     <Box sx={{ width: '100%', typography: 'body1' }}>
+//       <TabContext value={value}>
+//         <Box sx={{ borderBottom: 1, borderColor: 'divider', display: 'flex', justifyContent: 'center' }}>
+//           <TabList onChange={handleChange} aria-label="lab API tabs example">
+//             <Tab label="Granada" value="Granada" />
+//             <Tab label="Málaga" value="Malaga" />
+//           </TabList>
+//         </Box>
+//         <TabPanel value="Granada">
+//         </TabPanel>
+//         <TabPanel value="Malaga">
+//           < MapMalaga />
+//         </TabPanel>
+//       </TabContext>
+//       <div style={{ position:'absolute', top:'10px', right:'300px' }} >
+//         <div style={{ display:'flex', justifyContent:'center', alignItems:'center' }}>
+//           <Typography><b>{title} ({units})</b></Typography>
+//         </div>
+//       </div>
+//       <div style={{ position:'absolute', top:'0px', right:'20px' }} >
+//         <div style={{ display:'flex', justifyContent:'center', alignItems:'center' }}>
+//           <Typography variant="caption" color="text.secondary" align="center">Financiado por </Typography>
+//           <Sponsor />
+//         </div>
+//       </div>
+//       <div style={{ position:'absolute', top:'0px', left:'20px',margin:'2px' }} >
+//           <Developer />
+//       </div>
+//       <Box sx={{ position:'absolute', bottom:'60px', left:'20px'}}>
+//         <CardInfo />
+//         <Select />
+//         <SelectBasemap />
+//       </Box>
+//     </Box>
+//   );
+// }
